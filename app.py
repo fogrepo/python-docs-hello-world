90781d1fc5759ad661746450cc85f3191d62b11f
John Ansell <anselljp1@gmail.com>
2021-10-23 14:47:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Big hello, Azure enthusiast!  Learning auto deployment of applications using Azure Deployment Center with Github integration!  Takes about 1.5 minutes from Github edit commit to deployment on web server."
+    return "A big hello, Azure enthusiast!  Learning auto deployment of applications using Azure Deployment Center with Github integration!  Takes about 1.5 minutes from Github edit commit to deployment on web server."
